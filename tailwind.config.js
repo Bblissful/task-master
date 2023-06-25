aeac3f4e78e30a1916cc1fd68d2ede502aaf5091
Bblissful <101343866+Bblissful@users.noreply.github.com>
2023-06-25 19:44:32 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        font1: ["Font1"],
+      },
+    },
   },
   plugins: [],
 }
